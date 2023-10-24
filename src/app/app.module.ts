6b34b7c950f9d677a84d714193246d57b5807c5a
Marcos <marcos.fontenla@hotmail.com>
2023-10-24 10:05:24 -0300

    (fix): fixing buttons styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -33,6 +35,8 @@ import { CarDialogComponent } from './components/car-dialog/car-dialog.component
     MatInputModule,
     MatToolbarModule,
     MatPaginatorModule,
+    MatButtonModule,
+    MatDividerModule,
     FormsModule,
     ReactiveFormsModule
   ],
